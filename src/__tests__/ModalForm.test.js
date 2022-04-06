ee01e40cdcac606749cb438478af649ce76b8d75
Eleonora Kazakova <abei-2005@yandex.ru>
2022-04-06 19:37:57 +0200

    Add test for  ModalForm

--- src/__tests__/ModalForm.test.js
+++ src/__tests__/ModalForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import ModalForm from "../components/ModalForm";
@@ -10,8 +10,31 @@ const renderedForm = (
   <ModalForm createdItem={fakeCreatedItem} modalState={fakeModalState} />
 );
 
-test("Should render input element", async () => {
-  render(renderedForm);
-  const item = screen.getByPlaceholderText(/ex.: ice cream/i);
-  expect(item).toBeInTheDocument();
+describe("", () => {
+  test("Should render input element", async () => {
+    render(renderedForm);
+    const item = screen.getByPlaceholderText(/ex.: ice cream/i);
+    expect(item).toBeInTheDocument();
+  });
+
+  test("Should be able to tipe in input Item row", async () => {
+    render(renderedForm);
+    const item = screen.getByPlaceholderText(/ex.: ice cream/i);
+    fireEvent.change(item, { target: { value: "apple" } });
+    expect(item.value).toBe("apple");
+  });
+
+  test("Should be able to tipe in input Price row", async () => {
+    render(renderedForm);
+    const item = screen.getByPlaceholderText(/ex.: \$5/i);
+    fireEvent.change(item, { target: { value: "6" } });
+    expect(item.value).toBe("6");
+  });
+
+  test("Should be able to tipe in input Price row", async () => {
+    render(renderedForm);
+    const item = screen.getByPlaceholderText(/ex.: \$5/i);
+    fireEvent.change(item, { target: { value: "a6" } });
+    expect(item.value).not.toBe();
+  });
 });
